64ef92330b57f1c7c13ef4a096e7789e5c763fdd
Jannik Weise <mail@jannikweise.de>
2025-04-21 20:17:07 +0200

    Removed Burger Menu

--- app/src/main/java/de/janniqz/sustainabilitytracker/MainActivity.kt
+++ app/src/main/java/de/janniqz/sustainabilitytracker/MainActivity.kt
@@ -5,12 +5,10 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import de.janniqz.sustainabilitytracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -34,23 +32,6 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNav = binding.bottomNav
         bottomNav.setupWithNavController(navController)
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
