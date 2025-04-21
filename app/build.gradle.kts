44cbc90bd1167ab9520dc18a3942cfa2abf88085
Jannik Weise <mail@jannikweise.de>
2025-04-21 03:06:37 +0200

    Updated Packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "de.janniqz.sustainabilitytracker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "de.janniqz.sustainabilitytracker"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
