922eba81d3a903ea2a0ed424a686b5c0d3e7abc6
Jannik Weise <jannik.weise@funkemedien.de>
2025-04-23 11:54:04 +0200

    Added Create Task Design
    The Create Task Fragment now has two buttons for creating predefined / custom tasks.
    Currently neither Button does anything, purely design.
    
    The Buttons themselves have been outsourced into a separate component file and they're filled via the CreateTask class.

--- app/src/main/java/de/janniqz/sustainabilitytracker/ui/tasks/CreateTaskFragment.kt
+++ app/src/main/java/de/janniqz/sustainabilitytracker/ui/tasks/CreateTaskFragment.kt
@@ -6,11 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import de.janniqz.sustainabilitytracker.R
+import de.janniqz.sustainabilitytracker.databinding.FragmentCreateTaskBinding
 
 class CreateTaskFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_create_task, container, false)
+    private lateinit var binding: FragmentCreateTaskBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentCreateTaskBinding.inflate(inflater, container, false)
+
+        setupButtons()
+
+        return binding.root
+    }
+
+    private fun setupButtons() {
+        binding.btnPredefined.btnTitle.setText(R.string.task_type_predefined_title)
+        binding.btnPredefined.btnDesc.setText(R.string.task_type_predefined_desc)
+        binding.btnPredefined.btnIcon.setImageResource(R.drawable.ic_task_predefined)
+        binding.btnPredefined.btnRoot.setOnClickListener {
+
+        }
+
+        binding.btnCustom.btnTitle.setText(R.string.task_type_custom_title)
+        binding.btnCustom.btnDesc.setText(R.string.task_type_custom_desc)
+        binding.btnCustom.btnIcon.setImageResource(R.drawable.ic_task_custom)
+        binding.btnCustom.btnRoot.setOnClickListener {
+
+        }
     }
 
 }
