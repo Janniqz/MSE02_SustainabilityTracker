8ba0015c2552548679419119dc119c23a4974c56
Jannik Weise <jannik.weise@funkemedien.de>
2025-05-26 21:01:17 +0200

    Added Task Display Adapter
    Added a TaskDisplay Adapter so Tasks can actually be shown in the main Task Screen.
    Buttons already have functions assigned to them, although they don't do anything yet.

---
+++ app/src/main/java/de/janniqz/sustainabilitytracker/ui/tasks/TaskDisplayAdapter.kt
@@ -0,0 +1,45 @@
+package de.janniqz.sustainabilitytracker.ui.tasks
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import de.janniqz.sustainabilitytracker.R
+import de.janniqz.sustainabilitytracker.data.model.TaskCategory
+import de.janniqz.sustainabilitytracker.data.model.TaskType
+import de.janniqz.sustainabilitytracker.data.model.entity.TaskEntity
+import de.janniqz.sustainabilitytracker.databinding.ComponentTaskDisplayBinding
+
+class TaskDisplayAdapter(context: Context, tasks: List<TaskEntity>, private val taskEditFunc: (TaskEntity) -> Unit, private val taskCompleteFunc: (TaskEntity) -> Unit, private val taskDeleteFunc: (TaskEntity) -> Unit) : ArrayAdapter<TaskEntity>(context, 0, tasks) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding = if (convertView == null) {
+            ComponentTaskDisplayBinding.inflate(LayoutInflater.from(context), parent, false)
+        } else {
+            ComponentTaskDisplayBinding.bind(convertView)
+        }
+
+        val task = getItem(position)!!
+        val icon = when (task.category) {
+            TaskCategory.CO2 -> R.drawable.ic_category_co2
+            TaskCategory.Water -> R.drawable.ic_category_water
+            TaskCategory.Waste -> R.drawable.ic_category_waste
+        }
+
+        binding.taskName.text = task.name
+        binding.taskCategoryIcon.setImageResource(icon)
+
+        if (task.type == TaskType.Predefined) {
+            binding.taskDescription.text = task.description
+        } else {
+            binding.taskDescription.text = ""
+        }
+
+        binding.buttonEdit.setOnClickListener { taskEditFunc(task) }
+        binding.buttonComplete.setOnClickListener { taskCompleteFunc(task) }
+        binding.buttonDelete.setOnClickListener { taskDeleteFunc(task) }
+
+        return binding.root
+    }
+}
--- app/src/main/java/de/janniqz/sustainabilitytracker/ui/tasks/TasksFragment.kt
+++ app/src/main/java/de/janniqz/sustainabilitytracker/ui/tasks/TasksFragment.kt
@@ -1,20 +1,28 @@
 package de.janniqz.sustainabilitytracker.ui.tasks
 
+import AppDatabase
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import de.janniqz.sustainabilitytracker.R
+import de.janniqz.sustainabilitytracker.data.model.entity.TaskEntity
 import de.janniqz.sustainabilitytracker.databinding.FragmentTasksBinding
+import kotlinx.coroutines.launch
 
 class TasksFragment : Fragment() {
 
     private lateinit var binding: FragmentTasksBinding
+    private lateinit var taskListAdapter: ArrayAdapter<TaskEntity>
+    private lateinit var database: AppDatabase
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentTasksBinding.inflate(inflater, container, false)
+        database = AppDatabase.getInstance(requireContext())
         return binding.root
     }
 
@@ -22,6 +30,23 @@ class TasksFragment : Fragment() {
         binding.fabNewTask.setOnClickListener { view ->
             findNavController().navigate(R.id.action_createTask)
         }
+
+        lifecycleScope.launch {
+            taskListAdapter = TaskDisplayAdapter(requireContext(), database.task().getAll(), ::onEditTask, ::onCompleteTask, ::onDeleteTask)
+            binding.taskList.adapter = taskListAdapter
+        }
+    }
+
+    private fun onEditTask(task: TaskEntity) {
+        // TODO
+    }
+
+    private fun onCompleteTask(task: TaskEntity) {
+        // TODO
+    }
+
+    private fun onDeleteTask(task: TaskEntity) {
+        // TODO
     }
 
 }
